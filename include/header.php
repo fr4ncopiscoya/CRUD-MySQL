ead42b0a1c3786dea3e4d169aee74a14c9b5e9ac
Franco Piscoya <francomazns@gmail.com>
2023-07-06 19:22:09 -0500

    pt1 CRUD

---
+++ include/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>CRUD-MySQL</title>
+</head>
+<body>
+
+<nav class= "navbar navbar-dark bg-dark">
+    <div class="container">
+        <a href="index.php" class="navbar-brand">PHP MYSQL CRUD</a>
+    </div>
+
+</nav>
---
+++ index.php
@@ -0,0 +1,61 @@
+<?php include ("conexion.php") ?>
+
+<?php include ("include/header.php") ?>
+
+    <div class="container p-4">
+        <div class="row">
+            <div class="col-md-4">
+
+                <?php if(isset($_SESSION['message'])) {?>
+                    <div class="alert alert-<?= $_SESSION['message_type']  ?> alert-dismissible fade show" role="alert">
+                    <?= $_SESSION['message']  ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                <?php session_unset(); }?>
+
+                <div class="card card-body">
+
+                    <form action="save_task.php" method= "POST">
+                        <div class="form-group">
+                            <input type="text " name= "title" class = "form-control" placeholder= "Task Title" autofocus>
+                        </div>
+
+                        <div class="form-group">
+                            <textarea name="desciption" rows="5" class= "form-control" placeholder= "Task Description"></textarea>
+                        </div>
+                        
+                        <input type="submit" class= "btn btn-success btn-block" name="save_task" value= "Save Task">
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <table class = "table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Description</th>
+                            <th>Created At</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $query = "SELECT * FROM task";
+                        $result_tasks = mysqli_query($con,$query);  
+
+                        while($row = mysqli_fetch_array($result_tasks)){ ?>
+                            <tr>
+                                <td><?php echo $row['tsk_title'] ?></td>
+                                <td><?php echo $row['tsk_description'] ?></td>
+                                <td><?php echo $row['tsk_created'] ?></td>
+                            </tr>
+
+                        <?php } ?>
+                        
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+<?php include ("include/footer.php") ?>
